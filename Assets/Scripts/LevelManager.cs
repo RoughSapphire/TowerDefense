89ebd62e88087a1f0e42daf396d68b0328e9b85e
midniteshadow <marcanthonypalmer@gmail.com>
2017-10-18 10:09:00 -0500

    Add files via upload

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class LevelManager : MonoBehaviour {
+	[SerializeField]
+	private GameObject[] tilePrefabs;
+	public float TileSize{
+		get{return tilePrefabs[0].GetComponent<SpriteRenderer> ().sprite.bounds.size.x;}
+	}
+	// Use this for initialization
+	void Start () {
+		CreateLevel ();
+
+		}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void CreateLevel(){
+		Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height));
+		string[] mapData=ReadLevelText();
+		int mapX = mapData[0].ToCharArray().Length;
+		int mapY = mapData.Length;
+
+		for (int y = 0; y < mapY; y++) {
+			char[] newTiles = mapData[y].ToCharArray();
+			for (int x = 0; x < mapX; x++) {
+				PlaceTile (newTiles[y].ToString(),x, y,worldStart);
+
+			}
+		}
+	}
+	private void PlaceTile(string TileType, int x , int y,Vector3 worldStart){
+		int tileIndex = int.Parse(TileType);
+		TileScript newTile = Instantiate (tilePrefabs[tileIndex]).GetComponent<TileScript>();
+		newTile.transform.position = new Vector3 (worldStart.x + (TileSize * x), worldStart.y -(TileSize * y),0);
+		newTile.Setup (new Point (x, y));
+	}
+	private string[] ReadLevelText (){
+		TextAsset Binddata = Resources.Load("Level") as TextAsset;
+		string data = Binddata.text.Replace (Environment.NewLine, string.Empty);
+			return data.Split('-');
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+	public Point GridPosition{get;set;}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Setup(Point gridPos, Vector3 worldPos){
+		this.GridPosition = GridPosition;
+		transform.position = worldPos;
+	
+	}
+
+}
